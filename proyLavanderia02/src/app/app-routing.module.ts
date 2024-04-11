195b7a399300a66c7e2b77bdd18c4cd3b8bfd5e9
Yadimir Covarrubias <yadimir.cova@gmail.com>
2024-04-10 21:32:45 -0600

    se configuraron las rutas y el header

--- proyLavanderia02/src/app/app-routing.module.ts
+++ proyLavanderia02/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'nota-nueva', component: NotaNuevaComponent },
+    { path: 'logIn', component: LogInComponent },
+    { path: 'control-maquinas', component: ControlMaquinasComponent },
+  ];
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
