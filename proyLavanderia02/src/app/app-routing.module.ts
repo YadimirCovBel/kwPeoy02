f59b33080cbaf84e027eac864446f42c95158f81
Yadimir Covarrubias <yadimir.cova@gmail.com>
2024-04-10 21:42:14 -0600

    se configuro app-routing.module.ts

--- proyLavanderia02/src/app/app-routing.module.ts
+++ proyLavanderia02/src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NotaNuevaComponent } from './components/nota-nueva/nota-nueva.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { ControlMaquinasComponent } from './components/control-maquinas/control-maquinas.component';
 
 const routes: Routes = [
-  const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'nota-nueva', component: NotaNuevaComponent },
     { path: 'logIn', component: LogInComponent },
     { path: 'control-maquinas', component: ControlMaquinasComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
   ];
   
-];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
