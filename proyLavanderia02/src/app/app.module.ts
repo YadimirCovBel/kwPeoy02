0f7f19911a8e41cf5d1197cadea46c57d3dc2c16
Yadimir Covarrubias <yadimir.cova@gmail.com>
2024-04-11 18:06:33 -0600

    se agrego HttpClientModule en app.module.ts pero noe sta detectandolo como deberia aun

--- proyLavanderia02/src/app/app.module.ts
+++ proyLavanderia02/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -11,6 +12,7 @@ import { ControlMaquinasComponent } from './control-maquinas/control-maquinas.co
 
 @NgModule({
   declarations: [
+    HttpClientModule,
     AppComponent,
     HomeComponent,
     NotaNuevaComponent,
