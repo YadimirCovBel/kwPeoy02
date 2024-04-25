6777f7bddfc0b347be5bfd8ee9adeecac7508a37
Yadimir Covarrubias <yadimir.cova@gmail.com>
2024-04-25 17:39:56 -0600

    se agregaron los servicios para el backend

--- proyLavanderia02/src/app/nota-nueva/nota-nueva.component.ts
+++ proyLavanderia02/src/app/nota-nueva/nota-nueva.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-nota-nueva',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./nota-nueva.component.css']
 })
 export class NotaNuevaComponent {
+  servicioSeleccionado = '';
+  cantidad = 0;
+  cantidadSeleccionada = '';
 
+  constructor(private http: HttpClient){}//servicio
+  agregarCantidad() {
+    this.cantidadSeleccionada = `Cantidad seleccionada: ${this.cantidad}`;
+  
+}
+
+enviarFormulario() {
+  // Aquí puedes enviar los datos a tu backend
+  // Puedes usar HttpClient para hacer una petición HTTP a tu servidor
+  const datos = {
+    servicio: this.servicioSeleccionado,
+    cantidad: this.cantidad
+  };
+  // Realizar la solicitud POST al backend (reemplaza la URL con la tuya)
+  this.http.post('http://tu-url-de-backend.com', datos).subscribe(res => {
+    console.log(res); // Manejar la respuesta del backend aquí
+  });
+
+}
 }
