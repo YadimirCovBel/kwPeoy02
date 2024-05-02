8495fe29682e5dbc54b164fca464c5cdff722752
Yadimir Covarrubias <yadimir.cova@gmail.com>
2024-05-02 16:54:37 -0600

    se corrigieron errores de sintaxis dentro de server.js

--- backendNotaNueva/server.js
+++ backendNotaNueva/server.js
@@ -1,28 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.use(bodyparser.json());
+app.use(bodyParser.json());
 
-mongoose.connect('mongodb:/localhost:27017/baseDatosNotaNueva', {
+mongoose.connect('mongodb://localhost:27017/baseDatosNotaNueva', {
     useNewUrlParser: true,
-    unseUnifiedTopology: true
+    useUnifiedTopology: true 
 });
-
-const ServicioSchema = mongoose.Schema({
-    cliente:String,
-    servicios: [{ servicio:String, cantidad: Number}]
+//definision del esquema mongoose
+const ServicioSchema = new mongoose.Schema({
+    cliente: String,
+    servicios: [{ servicio: String, cantidad: Number }]
 });
-const Servicios = mongoose.model('Servicio', ServicioSchema);
+//modelo 
+const Servicio = mongoose.model('Servicio', ServicioSchema);
 
 app.post('/servicios', async (req, res) => {
-    const nuevoServicio = new ServicioSchema(req.body);
+    const nuevoServicio = new Servicio(req.body);
     await nuevoServicio.save();
     res.status(201).send('Servicio guardado');
-;})
+});
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
