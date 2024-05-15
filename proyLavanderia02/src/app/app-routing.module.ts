4404d85d51685b597f175e033c5d77ceb6e64201
Yadimir Covarrubias <yadimir.cova@gmail.com>
2024-05-14 19:06:31 -0600

    se agregaron las rutas de acumulado-cliente y lista-precios

--- proyLavanderia02/src/app/app-routing.module.ts
+++ proyLavanderia02/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { NotaNuevaComponent } from './nota-nueva/nota-nueva.component';
 import { ControlMaquinasComponent } from './control-maquinas/control-maquinas.component';
+import { AcumuladoClienteComponent } from './acumulado-cliente/acumulado-cliente.component';
+import { ListaPreciosComponent } from './lista-precios/lista-precios.component';
 
 const routes: Routes = [
   {path:'home', component: HomeComponent},
   {path: 'nota-nueva', component: NotaNuevaComponent},
   {path: 'login', component: LogInComponent},
   {path: 'control-maquinas', component: ControlMaquinasComponent},
+  {path: 'acumulado-cliente', component: AcumuladoClienteComponent},
+  {path: 'lista-precios', component: ListaPreciosComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
