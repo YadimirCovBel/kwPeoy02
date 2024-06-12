5ac1b64b43855fa765f7411a96457e78238a2ec5
Yadimir Covarrubias <yadimir.cova@gmail.com>
2024-06-11 19:40:40 -0600

    dentro del 02Bash.sh cambiamos los comandos quitamos rutas absolutas y dimos otros comandos a los arranques de mongo, expres y angular

--- pre-config/02bash.sh
+++ pre-config/02bash.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/bash
 
-echo leyendo directorio...
+echo "Leyendo directorio..."
 ls > procesos.txt
-echo archivo generado satisfactoriamente
+echo "Archivo generado satisfactoriamente"
 
-echo leyendo informacion de red...
+echo "Leyendo información de red..."
 ip addr > redes.txt
-echo archivo de redes creado satisfactoriamente
+echo "Archivo de redes creado satisfactoriamente"
 
 
-# Arrancar MongoDB en una nueva terminal
+
+# Arrancar MongoDB
 echo "Arrancando MongoDB..."
-gnome-terminal -- bash -c "cd '/home/yadimir/Desktop/kwProy02/backend'; sudo systemctl start mongod; sudo systemctl daemon-reload; sudo systemctl status mongod; exec bash"
+sudo systemctl start mongod
+sudo systemctl daemon-reload
+sudo systemctl status mongod
 
-# Arrancar Express en una nueva terminal
+# Arrancar Express
 echo "Arrancando Express..."
-gnome-terminal -- bash -c "cd '/ruta/al/directorio/de/express'; node server.js; exec bash"
+cd /ruta/al/directorio/de/express
+node server.js &
 
-# Arrancar servidor Angular en una nueva terminal
+# Arrancar servidor Angular
 echo "Arrancando servidor Angular..."
-gnome-terminal -- bash -c "cd '/home/yadimir/Desktop/kwProy02/proyLavanderia02'; sudo serve -l 3008 -s /dist/proy-lavanderia02/; exec bash"
+cd /home/yadimir/Desktop/kwProy02/proyLavanderia02/dist/proy-lavanderia02
+http-server -p 3008 &
 
-echo "Todos los servicios han sido iniciados en terminales separadas."
+echo "Todos los servicios han sido iniciados."
