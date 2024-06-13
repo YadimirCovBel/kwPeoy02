6f3afc48f70b2eeb7f730da0722dcdf9de61f944
Yadimir Covarrubias <yadimir.cova@gmail.com>
2024-06-12 18:19:34 -0600

    se escribieron los comandos para instalar lo basico para el funcionamiento de la rasberry pi, tambien la extraccion de key y clonado de repo

--- pre-config/02bash.sh
+++ pre-config/02bash.sh
@@ -11,6 +11,41 @@ echo "Archivo de redes creado satisfactoriamente"
 export SYSTEMD_DIRECTORY=/home/ubuntu/kwProy02/pre-config
 export SYSTEMD_START=/bin/bash /home/ubuntu/kwProy02/pre-config/02bash.sh
 
+# instalar basicos en Rasberry pi
+echo "actualizando..."
+sudo apt update
+echo "instalando build esential..."
+sudo apt install buid-esential
+buid-esential -v
+echo "instalando wiring pi..."
+sudo apt -y install wiringpi
+wiringpi -v
+echo "instalando node.js..."
+sudo apt install node.js
+node -v
+echo "instalando git..."
+sudo apt install git
+git -v
+echo "instalando python..."
+sudo apt install python3
+python3 -v
+echo "instalando npm..."
+sudo apt install npm
+npm -v
+echo "instalando ng..."
+sudo apt install ng-common
+ng-common -v
+
+
+# extracting key
+echo "extracting key..."
+ssh -keygen -t rsa
+cat id_rsa.pub
+
+# cloning git
+git clone https://github.com/YadimirCovBel/kwPeoy02.git
+git pull origin main pull
+
 # Arrancar MongoDB
 echo "Arrancando MongoDB..."
 sudo systemctl start mongod
