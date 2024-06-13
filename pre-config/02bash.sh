b6c2e9aa23ec69f79b6b2973aeae77a0684cc4dd
Yadimir Covarrubias <yadimir.cova@gmail.com>
2024-06-12 19:18:51 -0600

    los comandos de instalacion y verificasion fueron modificados y se agrego echo de que fue instalado, tambien se cambio en la extraccion de key los comandos, dentro la clonacion se cambio el comando de pull, edision de rutas en arrancar expres y servidor de angular

--- pre-config/02bash.sh
+++ pre-config/02bash.sh
@@ -14,40 +14,67 @@ export SYSTEMD_START=/bin/bash /home/ubuntu/kwProy02/pre-config/02bash.sh
 # instalar basicos en Rasberry pi
 echo "actualizando..."
 sudo apt update
-echo "instalando build esential..."
-sudo apt install buid-esential
-buid-esential -v
+echo "Update satisfactoriamente..."
+
+echo "instalando build esential ..."
+sudo apt install -y build-essential
+build-essential -v
+echo "buid-esential instalando satisfactoriamente..."
+
 echo "instalando wiring pi..."
 sudo apt -y install wiringpi
-wiringpi -v
+gpio -v
+echo "wiringpi instalando satisfactoriamente..."
+
 echo "instalando node.js..."
-sudo apt install node.js
+sudo apt install -y nodejs
 node -v
+echo "nodejs instalando satisfactoriamente..."
+
+echo "instalando http-server..."
+npm install -g http-server
+echo "http-server instalando satisfactoriamente..."
+
+
 echo "instalando git..."
-sudo apt install git
-git -v
+sudo apt install -y git
+git --version
+echo "git instalando satisfactoriamente..."
+
 echo "instalando python..."
-sudo apt install python3
-python3 -v
+sudo apt install -y python3
+python3 --version
+echo "python3 instalando satisfactoriamente..."
+
 echo "instalando npm..."
-sudo apt install npm
+sudo apt install -y npm
 npm -v
+echo "npm instalando satisfactoriamente..."
+
+echo "Instalando Angular CLI..."
+sudo npm install -g @angular/cli
+ng version
+echo "Angular instalando satisfactoriamente..."
+
 echo "instalando ng..."
 sudo apt install ng-common
 ng-common -v
+echo "ng-common instalando satisfactoriamente..."
+
 echo "todos los basicos han sido instalandos satisfactoriamente..."
 
 
-# extracting key
+# extracting ssh key
 echo "extracting key..."
-ssh -keygen -t rsa
-cat id_rsa.pub
+ssh-keygen -t rsa -q -f "$HOME/.ssh/id_rsa" -N ""
+cat "$HOME/.ssh/id_rsa.pub"
 echo "the key has been extracted satisfactorily ..."
 
 # cloning git
 echo "cloning repo..."
 git clone https://github.com/YadimirCovBel/kwPeoy02.git
-git pull origin main pull
+cd kwPeoy02
+git pull origin main
 echo "the repo has been cloned satisfactorily ..."
 
 # Arrancar MongoDB
@@ -58,12 +85,12 @@ sudo systemctl status mongod
 
 # Arrancar Express
 echo "Arrancando Express..."
-cd /ruta/al/directorio/de/express
+cd /home/ubuntu/kwProy02/backend/server.js
 node server.js &
 
 # Arrancar servidor Angular
 echo "Arrancando servidor Angular..."
-cd /home/yadimir/Desktop/kwProy02/proyLavanderia02/dist/proy-lavanderia02
+cd /home/ubuntu/kwProy02/proyLavanderia02/dist/proy-lavanderia02
 http-server -p 3008 &
 
 echo "Todos los servicios han sido iniciados."
