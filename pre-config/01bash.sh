18fc8880126177e6fbf21a420ab7b6d0852a0e04
Yadimir Covarrubias <yadimir.cova@gmail.com>
2024-06-13 21:09:58 -0600

    se modifico la instalacion de paquetes basicos para el funcionamiento de la rasberry pi en este caso creamos install_package donde definimos que revisara si estan instalados y de no ser asi isntalarlos para despues,  esta definido abajo donde indicamos todos los paquetes ocupados

--- pre-config/01bash.sh
+++ pre-config/01bash.sh
@@ -19,53 +19,41 @@ echo "Archivo de redes creado satisfactoriamente"
 echo "Actualizando..."
 sudo apt update && echo "Update satisfactoriamente..."
 
+# Función para verificar si un paquete está instalado
+is_installed() {
+    dpkg -l "$1" &> /dev/null
+}
 
-echo "instalando build esential ..."
-sudo apt install -y build-essential
-build-essential -v
-echo "buid-esential instalando satisfactoriamente..."
+# Instalar un paquete si no está instalado
+install_package() {
+    if ! is_installed "$1"; then
+        echo "Instalando $1..."
+        sudo apt install -y "$1" && echo "$1 instalado satisfactoriamente..."
+    else
+        echo "$1 ya está instalado."
+    fi
+}
 
-echo "instalando WiringPi..."
-sudo apt -y install wiringpi
-gpio -v
-echo "WiringPi instalando satisfactoriamente..."
+# Instalar paquetes
+install_package "build-essential"
+install_package "wiringpi"
+install_package "nodejs"
+install_package "git"
+install_package "python3"
+install_package "npm"
+install_package "ng-common"
 
-echo "instalando node.js..."
-sudo apt install -y nodejs
-node -v
-echo "nodejs instalando satisfactoriamente..."
+# Instalar http-server si no está disponible
+if ! command -v http-server &> /dev/null; then
+    echo "Instalando http-server..."
+    npm install -g http-server && echo "http-server instalado satisfactoriamente..."
+fi
 
-echo "instalando http-server..."
-npm install -g http-server
-echo "http-server instalando satisfactoriamente..."
-
-
-echo "instalando git..."
-sudo apt install -y git
-git --version
-echo "git instalando satisfactoriamente..."
-
-echo "instalando python..."
-sudo apt install -y python3
-python3 --version
-echo "python3 instalando satisfactoriamente..."
-
-echo "instalando npm..."
-sudo apt install -y npm
-npm -v
-echo "npm instalando satisfactoriamente..."
-
-echo "Instalando Angular CLI..."
-sudo npm install -g @angular/cli
-ng version
-echo "Angular instalando satisfactoriamente..."
-
-echo "instalando ng..."
-sudo apt install ng-common
-ng-common -v
-echo "ng-common instalando satisfactoriamente..."
-
-echo "todos los basicos han sido instalandos satisfactoriamente..."
+# Instalar Angular CLI si no está disponible
+if ! command -v ng &> /dev/null; then
+    echo "Instalando Angular CLI..."
+    sudo npm install -g @angular/cli && echo "Angular CLI instalado satisfactoriamente..."
+fi
 
 
 # extracting ssh key
