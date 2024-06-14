da61f893a8bac9d24bf2826c69cf5f50f1530aff
Yadimir Covarrubias <yadimir.cova@gmail.com>
2024-06-13 21:17:53 -0600

    se generaron ecos de arranque para cada servicio

--- pre-config/02bash.sh
+++ pre-config/02bash.sh
@@ -10,15 +10,18 @@ echo "Arrancando MongoDB..."
 sudo systemctl start mongod
 sudo systemctl daemon-reload
 sudo systemctl status mongod
+echo "MongoDB Arrancado satisfactoriamente..."
 
 # Arrancar Express
 echo "Arrancando Express..."
 cd /home/ubuntu/kwProy02/backend/server.js
 node server.js &
+echo "Express Arrancado satisfactoriamente  ..."
 
 # Arrancar servidor Angular
 echo "Arrancando servidor Angular..."
 cd /home/ubuntu/kwProy02/proyLavanderia02/dist/proy-lavanderia02
 http-server -p 3008 &
+echo "Angular Arrancado satisfactoriamente  ..."
 
 echo "Todos los servicios han sido iniciados."
